4a58b363bc3d01e0cafdf477c8c1d1d3c56704de
joaocandidozup <joao.candido@zup.com>
2024-12-10 10:52:44 -0300

    feat: creates the disordered vector

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] vetor = {10,9,8,7,6,5,4,3,2,1,0};
+        System.out.print("vetor desordenado = ");
+        for(int i: vetor){
+            System.out.print(i + " ");
+        }
     }
 }
